1c066de61169bf6a1f365129dcfce6e45f229001
Manikanta Vedula <manikantavedula7@gmail.com>
2023-07-23 22:32:39 +0530

    Updates

---
+++ static/js/324.1a3ec928.chunk.js
@@ -0,0 +1,2 @@
+"use strict";(self.webpackChunkinfopublishers=self.webpackChunkinfopublishers||[]).push([[324],{30324:function(e,s,n){n.r(s);var r=n(80184);s.default=function(){return(0,r.jsx)("div",{children:"PrepareTest"})}}}]);
+//# sourceMappingURL=324.1a3ec928.chunk.js.map
--- static/js/324.d1de07f2.chunk.js
+++
@@ -1,2 +0,0 @@
-"use strict";(self.webpackChunkinfopublishers=self.webpackChunkinfopublishers||[]).push([[324],{324:function(e,s,n){n.r(s);var r=n(184);s.default=function(){return(0,r.jsx)("div",{children:"PrepareTest"})}}}]);
-//# sourceMappingURL=324.d1de07f2.chunk.js.map
