52517512cdc468d6e57ae6f130fcacda9fe3e070
Manikanta Vedula <manikantavedula7@gmail.com>
2024-03-03 03:30:46 +0530

    new

--- backend/api/subject/fetch.py
+++ backend/api/subject/fetch.py
@@ -1,5 +1,36 @@
 from connection import connect_to_db
 
+def get_data_by_standard(standard):
+    mycursor = None
+
+    mydb = connect_to_db()
+    
+    if mydb.is_connected():
+        print("Connection successful")
+    else:
+        print("Connection failed")
+
+    mycursor = mydb.cursor()
+
+    results = []
+    
+    if mydb.is_connected():
+        mycursor.execute("SELECT s.* FROM `lesson` l, `subject` s WHERE l.standard = %s AND l.subject = s.id GROUP BY s.name", (standard,))
+        rows = mycursor.fetchall()
+        
+        # get the column names
+        col_names = [description[0] for description in mycursor.description]
+
+        # create a list of dictionaries where each dictionary represents a row with column names as keys and row values as values
+        results = [dict(zip(col_names, row)) for row in rows]
+
+        mycursor.close()
+        mydb.close()
+    else:
+        mycursor = None
+
+    return results
+
 def get_data():
     mycursor = None
 
--- backend/api/subject/routes.py
+++ backend/api/subject/routes.py
@@ -1,5 +1,5 @@
 from flask import jsonify, make_response, Blueprint, request
-from .fetch import get_data
+from .fetch import get_data, get_data_by_standard
 from .post import post_data
 from .update import update_data
 from .delete import delete_data
@@ -24,6 +24,31 @@ handler.setFormatter(formatter)
 logger.addHandler(handler)
 
 subject_routes = Blueprint('subject', __name__, url_prefix='/subject')
+
+@subject_routes.route('/dataByStandard', methods=['POST', 'OPTIONS'])
+@cross_origin(supports_credentials=True)
+def get_data_by_standard_endpoint():
+    logger.debug(f'request: {request} {request.json}')
+    if request.method == 'OPTIONS':
+        headers = {
+            'Access-Control-Allow-Origin': 'https://app.infopublisher.in',
+            'Access-Control-Allow-Methods': 'POST',
+            'Access-Control-Allow-Headers': 'Access-Control-Allow-Headers, Access-Control-Allow-Methods, Access-Control-Allow-Origin, Access-Control-Allow-Credentials, Origin, X-Requested-With, Content-Type, Accept, Authorization, access-control-allow-credentials,access-control-allow-headers,access-control-allow-methods,access-control-allow-origin,content-type',
+            "Access-Control-Allow-Credentials": "true",
+        }
+        return ('', 204, headers)
+    
+    elif request.method == 'POST':
+        standard = request.json['standardID']
+        # status = request.json['status']
+
+        logger.debug(f'A message to log. {request.json} {standard}')
+
+        data = get_data_by_standard(standard)
+        response = make_response(jsonify(data))
+
+        return response
+    
 
 @subject_routes.route('/data')
 def get_data_endpoint():
